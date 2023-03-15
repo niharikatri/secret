5467c229ab7e8189418d6035aa463940015a0f08
niharika@preciousinfosystem.com <niharika@preciousinfosystem.com>
2023-03-15 12:13:47 +0000

    [1005602] create and forget password api

--- template-app/app/controllers/bx_block_forgot_password/passwords_controller.rb
+++ template-app/app/controllers/bx_block_forgot_password/passwords_controller.rb
@@ -75,7 +75,50 @@ module BxBlockForgotPassword
       end
     end
 
+    def create_password
+      if params[:new_password].present? && params[:token]
+        begin
+          token = BuilderJsonWebToken.decode params[:token]
+        rescue
+          return render json: { errors: [token: 'Token has Expired'] },
+          status: :unprocessable_entity
+        end
+
+        @account = AccountBlock::Account.find_by(id: token.id)
+
+        if @account.present? && @account.update_attribute(:password, params[:new_password])
+            render json: { status: true, data: @account }
+        else
+            render json: { status: false }
+        end
+      else
+        flash.now[:error] = 'Please fill all fields.'
+      end
+    end
+
+    def generate_password_link
+      if params[:data][:email].present?
+        @account = AccountBlock::Account.where(email: params[:data][:email].downcase).last
+        if @account.present?
+          forgot_password_email_for(@account)
+          render json: {
+            success:'Password link was successfully sent to the registered mail. Please check your email' 
+          }, status: :ok
+        else
+          render json: {
+            errors:"Account not found." 
+          }, status: :not_found
+        end
+      end
+    end
+
     private
+
+    def forgot_password_email_for account
+      BxBlockForgotPassword::ForgotPasswordMailer
+        .with(host: request.base_url)
+        .forgot_password_email(account).deliver
+    end
 
     def create_params
       params.require(:data)
--- template-app/config/routes.rb
+++ template-app/config/routes.rb
@@ -7,7 +7,16 @@ Rails.application.routes.draw do
     resources :accounts
   end
 
+  namespace :bx_block_login do
+    resources :logins
+  end
+
   namespace :bx_block_termsandconditions do
     resources :terms_and_conditions
+  end
+
+  namespace :bx_block_forgot_password do
+    get 'generate_password_link', to: 'passwords#generate_password_link'
+    put 'create_password', to: 'passwords#create_password'
   end
 end
