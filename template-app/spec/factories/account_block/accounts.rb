e0796eb3ff623a95ab62785ab339239bd2461122
niharika@preciousinfosystem.com <niharika@preciousinfosystem.com>
2023-03-16 12:43:04 +0000

    [1005602] sonar issue

--- template-app/spec/factories/account_block/accounts.rb
+++ template-app/spec/factories/account_block/accounts.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
     factory :account, class: AccountBlock::Account do
-      email { "test@gmail.com" }
+      first_name {"demo"}
+      last_name {"test"}
+     sequence(:email) { |n| "abc#{n}@example.com" }
       type {"EmailAccount"}
       password { "test@123" }
-      activated {true}
+      password_confirmation {"test@123"}
+      unique_auth_id { 123456 }
     end
 end
---
+++ template-app/spec/requests/bx_block_forgot_password/passwords_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe BxBlockForgotPassword::PasswordsController, type: :request do
+  #let(:account){FactoryBot.create(:account,email: "test@example.com")}
+  VAR=URI.encode("http://www.example.com:80/bx_block_forgot_password/generate_password_link")
+  VAR1=URI.encode("http://www.example.com:80/bx_block_forgot_password/create_password")
+  before(:each) do
+    @email_account = FactoryBot.create(:email_account)
+    @token = BuilderJsonWebToken.encode(@email_account.id)
+  end
+
+  describe "GET /generate_password_link" do
+    context 'when there is a request to forgot password generate' do
+      it 'When password link successfully generate' do
+        get VAR, params: { data: { email: @email_account.email,activated: true }, :format => 'js' }
+        expect(response.status).to eq 200
+      end
+     
+      it "When password link successfully generate" do
+        get VAR, params: { data: { email: @email_account.email }, :format => 'js' }
+      end
+     
+      it "When password not" do
+        get VAR, params: { data: { email: "nil_data@yopmail.com" }  }
+        expect(response.status).to eq 404
+      end  
+    end
+  end
+
+   describe "PUT /create_password" do
+     context 'reset password' do
+      it 'create new password through link' do
+        put VAR1, params: { new_password: "test@123" ,token: @token }
+        expect(response.status).to eq 200
+      end
+      
+      it 'create new password through link' do
+        put VAR1, params: { new_password: "test@123" ,token: ".." }
+        expect(response.status).to eq 422
+      end
+
+      it 'create new password through link' do
+        put VAR1, params: {  new_password: nil }
+        expect(response.status).to eq 204
+      end
+    end
+  end
+end
