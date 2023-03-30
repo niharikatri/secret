7cd93e196d9ad3bf9c9a8e62c74248cdfa78f652
pulkit.r@scaleus.in <pulkit.r@scaleus.in>
2023-03-30 08:03:37 +0000

    [1026291] Rspec fix

--- template-app/app/models/account_block/character.rb
+++ template-app/app/models/account_block/character.rb
@@ -3,6 +3,6 @@ module AccountBlock
   	self.table_name = :characters
   	has_one_attached :image
   	validates :name, presence: true, uniqueness: true
-  	validates :image, attached: true, content_type: {in: ['image/png', 'image/jpeg, image/gif'], message: "Please select image or gif." }
+  	validates :image, attached: true, content_type: {in: ['image/png', 'image/jpeg, image/gif', 'gif'], message: "Please select image or gif." }
   end
 end
--- template-app/app/models/account_block/voice.rb
+++ template-app/app/models/account_block/voice.rb
@@ -3,6 +3,6 @@ module AccountBlock
   	self.table_name = :voices
   	has_one_attached :audio
   	validates :name, presence: true, uniqueness: true
-  	validates :audio, attached: true, content_type: { in: [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio', 'audio/wav' ], message: "Please select audio file." }
+  	validates :audio, attached: true, content_type: { in: [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio', 'audio/wav', 'wav' ], message: "Please select audio file." }
   end
 end
